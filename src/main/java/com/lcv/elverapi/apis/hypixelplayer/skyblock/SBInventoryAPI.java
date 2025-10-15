6278d836a89d47b0128d76af6452ea4a0894a5ba
LeverClient <ah19890000@gmail.com>
2025-10-15 01:44:02 -0400

    added nedit lib

--- src/main/java/com/lcv/elverapi/apis/hypixelplayer/skyblock/SBInventoryAPI.java
+++ src/main/java/com/lcv/elverapi/apis/hypixelplayer/skyblock/SBInventoryAPI.java
@@ -1,17 +1,36 @@
 package com.lcv.elverapi.apis.hypixelplayer.skyblock;
 
 import com.lcv.elverapi.apis.SubApi;
+import me.nullicorn.nedit.NBTReader;
+import me.nullicorn.nedit.type.NBTCompound;
 import org.json.JSONObject;
 
-public class SBInventoryAPI extends SubApi {
+import java.io.IOException;
+
+public class SBInventoryAPI extends SubApi
+{
+
     public SBInventoryAPI(SkyblockAPI parent)
     {
         super(parent, "members." + parent.getUuidNoHyphen() + ".inventory");
         System.out.println(this.exists());
     }
 
+    private static String parseBase64(String s)
+    {
+        try
+        {
+            System.out.println(s);
+            return NBTReader.readBase64(s).toString();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
     public String getInventory()
     {
-        return (String) internalApiMap.computeIfAbsent("inventory", (k) -> get(null, "inv_content"));
+        return (String) internalApiMap.computeIfAbsent("inventory", (k) -> parseBase64(get(null, "inv_contents.data")));
     }
 }
